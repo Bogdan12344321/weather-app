93bb39480547b9f4dc8e877d1b94c0b270d80d99
Pitaroiu.Bogdan <bogdan.pitaroiu@doraly.ro>
2020-05-04 17:26:53 +0300

    Add about text

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,14 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
+
 weatherFrom.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const location = search.value;
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 return messageOne.textContent = data.error;
