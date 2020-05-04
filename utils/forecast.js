e4dcfc29786ade732dd32225ceaafd0fcd60d2ad
Pitaroiu.Bogdan <bogdan.pitaroiu@doraly.ro>
2020-05-04 18:06:39 +0300

    Works like a charm

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (longitude, latitude, callback) => {
-    const url = "http://api.weatherstack.com/current?access_key=0d5844c629f49b125863a2710670afce&query=" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude) + "&units=f";
+    const url = "http://api.weatherstack.com/current?access_key=0d5844c629f49b125863a2710670afce&query=" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude) + "&units=m";
     request({
         url,
         json: true
@@ -13,7 +13,8 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback("Unable to find location");
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is curently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out.`)
+
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is curently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out. Visibility is ${body.current.visibility}, and the humidity is ${body.current.humidity}`)
         }
     })
 }
